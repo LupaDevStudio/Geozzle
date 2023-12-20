a8f76ea773abb6c9e7c37a414d7b3f7b89905f85
Laure-Emilie <laure-emilie.martin@student-cs.fr>
2023-12-20 19:30:27 +0100

    fix screens colors

--- screens/game_question.py
+++ screens/game_question.py
@@ -20,7 +20,8 @@ from tools.path import (
 )
 from tools.constants import (
     DICT_CONTINENTS,
-    LIST_CONTINENTS
+    LIST_CONTINENTS,
+    TEXT
 )
 from tools.kivy_tools import ImprovedScreen
 
@@ -33,6 +34,7 @@ class GameQuestionScreen(ImprovedScreen):
 
     code_continent = StringProperty(LIST_CONTINENTS[0])
     continent_color = ColorProperty(DICT_CONTINENTS[LIST_CONTINENTS[0]])
+    text_label = StringProperty()
 
     def __init__(self, **kwargs) -> None:
         super().__init__(
@@ -61,3 +63,9 @@ class GameQuestionScreen(ImprovedScreen):
 
     def go_back_to_home(self):
         self.manager.current = "home"
+
+    def go_to_game_summary(self):
+        self.manager.current = "game_summary"
+
+    def update_labels(self):
+        self.text_label = TEXT.game_question["Choississez un nouvel indice"]
