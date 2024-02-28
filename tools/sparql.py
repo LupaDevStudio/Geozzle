d9539996a5acd7de460565af7fcb8d9b20ed0b3f
Laure-Emilie <laure-emilie.martin@student-cs.fr>
2024-02-28 14:34:19 +0100

    add Palestine

--- tools/sparql.py
+++ tools/sparql.py
@@ -524,5 +524,5 @@ def request_all_clues(wikidata_code_country: str, code_continent: str, language 
 if __name__ == "__main__":
     if BOOL_CREATE_DICT_CONTINENTS:
        for code_continent in DICT_WIKIDATA_CONTINENTS:
-           request_countries_continent(code_continent=code_continent, language="fr")
+           request_countries_continent(code_continent=code_continent, language="en")
     print(request_all_clues("Q236", "Europe"))
