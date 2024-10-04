29215f4686e3a2e3398aba61b9ac5c4775b4eab5
Agathe <lupa.dev.studio@gmail.com>
2024-10-04 16:37:53 +0200

    Fix init bug of musics

--- tools/__init__.py
+++ tools/__init__.py
@@ -13,15 +13,18 @@ from tools.path import (
 from tools.constants import (
     MAIN_MUSIC_NAME
 )
+from tools.geozzle import (
+    USER_DATA
+)
 from tools.game_tools import (
     DynamicMusicMixer,
     SoundMixer,
     load_sounds
 )
 
-MUSIC_DICT = load_sounds([MAIN_MUSIC_NAME + ".mp3"], PATH_MUSICS, 0.5)
-SOUND_DICT = load_sounds(["click" + ".mp3"], PATH_SOUNDS, 0.5)
+MUSIC_DICT = load_sounds([MAIN_MUSIC_NAME + ".mp3"], PATH_MUSICS, USER_DATA.music_volume)
+SOUND_DICT = load_sounds(["click" + ".mp3"], PATH_SOUNDS, USER_DATA.sound_volume)
 
 # Create the mixer
-music_mixer = DynamicMusicMixer(MUSIC_DICT, 0.5)
-sound_mixer = SoundMixer(SOUND_DICT, 0.5)
+music_mixer = DynamicMusicMixer(MUSIC_DICT, USER_DATA.music_volume)
+sound_mixer = SoundMixer(SOUND_DICT, USER_DATA.sound_volume)
